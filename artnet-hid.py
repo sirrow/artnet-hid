447033cf0d7ae87b072862102eb16e093d64303f
sirrow <sirrow@gmail.com>
2024-03-09 12:26:57 +0900

    compliant to hsgw protocol (sending hsv via Raw HID)

--- artnet-hid.py
+++ artnet-hid.py
@@ -9,16 +9,46 @@ usage_page    = 0xFF60
 usage         = 0x61
 report_length = 32
 
+
+def rgb_to_hsv(r, g, b):
+    """Convert RGB to HSV."""
+    r, g, b = r / 255.0, g / 255.0, b / 255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v
+
+def hsv_qmk_range(h, s, v):
+    """Convert HSV to QMK range."""
+    return int(h/360*255), int((s/100)*255), int((v/100)*255)
+
 # create a callback to handle data when received
 def test_callback(rdata):
     """Test function to receive callback data."""
     # the received data is an array
     # of the channels value (no headers)
     # print('Received new data \n', rdata)
-    data = rdata[0:3]
+    #data = rdata[0:3]
 
     request_data = [0x00] * (report_length + 1) # First byte is Report ID
-    request_data[1:len(data) + 1] = data
+    #request_data[1:len(data) + 1] = data
+    request_data[1] = 1 #HSV
+    h, s ,v = rgb_to_hsv(rdata[0], rdata[1], rdata[2])
+    request_data[2:5] = hsv_qmk_range(h, s, v)
+    print(request_data[2:5])
     request_report = bytes(request_data)
 
     device_interfaces = hid.enumerate(0, 0)
